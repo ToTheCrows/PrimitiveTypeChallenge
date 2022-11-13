605febdd035abb50e89815e95eb96bdf3af2c869
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-13 14:50:40 +0100

    basic structure

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         byte numByte = 10;
         short numShort = 20;
         int numInt = 50;
-        long numLong = 50000 + (10 * (numByte + numShort + numInt));
+        long numLong = 50000L + (10L * (numByte + numShort + numInt));
 
         System.out.println("numLong: "+numLong);
     }
