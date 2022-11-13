dc8e9515024074ba1d6a68c7f22da23a97071917
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-13 14:52:08 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
         byte numByte = 10;
         short numShort = 20;
         int numInt = 50;
+        //don`t forget the "L" for long declaration
         long numLong = 50000L + (10L * (numByte + numShort + numInt));
 
-        System.out.println("numLong: "+numLong);
+        System.out.println("numLong: " + numLong);
     }
 }
